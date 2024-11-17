42989ed6530d9dab0bf9c771703089f4c8902192
dinesh2003 <adhikaridinesh76@gmail.com>
2024-11-16 20:10:58 -0500

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  {
+    "rules": {
+      "semi": [
+        2,
+        `always`
+      ],
+      "array-bracket-spacing": [
+        2,
+        `always`
+      ],
+      "no-const-assign": 2,
+      "no-var": `error`,
+      "indent": [
+        2,
+        2
+      ],
+      "quotes": [
+        2,
+        `backtick`
+      ],
+      "global-require": 2,
+      "no-console": [
+        0
+      ],
+      "no-unused-vars": `off`,
+      "no-constant-condition": `off`
+    }
+  }
+];
