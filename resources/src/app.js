ef601e3a5e9469899013366bfdb04cd3924963aa
dinesh2003 <adhikaridinesh76@gmail.com>
2024-11-17 23:53:59 -0500

    fixed small issues

---
+++ resources/src/app.js
@@ -0,0 +1,23 @@
+//const ExifReader = require('exifreader');
+
+const selectButton = document.getElementById('select-button');
+
+let chosenFile;
+
+selectButton.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  let input = document.createElement('input');
+  input.type = 'file';
+  input.onchange = () => {
+    chosenFile = Array.from(input.files);
+    console.log(chosenFile);
+  };
+  input.click();
+});
+
+
+
+
+
+
