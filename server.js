d98b9f797ce3d94c62bad0b4b629ef755f949576
dinesh2003 <adhikaridinesh76@gmail.com>
2024-12-19 01:11:36 -0500

    post request for image working

--- server.js
+++ server.js
@@ -6,31 +6,21 @@ import fs from "fs";
 const PORT = 3000;
 const app = express();
 
-const File_Name = `FILE_UPLOADED`;
-
 app.use(express.static(path.resolve(import.meta.dirname, `public`)));
 app.use(express.static(path.resolve(import.meta.dirname, `src`)));
 app.use(express.static(path.resolve(import.meta.dirname, `dist`)));
 
-const upload = multer({dest: path.resolve(import.meta.dirname, `public`)});
+const upload = multer({dest: path.resolve(import.meta.dirname, `./images`)});
 
-
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  return next();
-});
-
+/*
 app.get(`/` ,(req, res, ) => {
   return res.send(`index.html`);
-});
+});*/
 
-app.post(`/public/images`, upload.single(`file`), (req, res) => {
-  res.send(`uploaded successfully`);
+app.post(`/api/post/image`, upload.single(`image`), (req, res) => {
+  res.send(`file received`);
+  console.log(req.file, req.body);
 });
-
-//app.get(`./images`, async(req, res) => {
-//  const images = await fs.promises.readdir(path.resolve(import.meta.dirname, `public`));
-//});
 
 app.listen(PORT, () => 
 {
