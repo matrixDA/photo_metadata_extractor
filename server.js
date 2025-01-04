6a9fb4fb9fa94336982aa5a3201153c59f865ebd
dinesh2003 <adhikaridinesh76@gmail.com>
2025-01-04 15:37:58 -0500

    added additional code in server.js

--- server.js
+++ server.js
@@ -10,15 +10,31 @@ app.use(express.static(path.resolve(import.meta.dirname, `public`)));
 app.use(express.static(path.resolve(import.meta.dirname, `src`)));
 app.use(express.static(path.resolve(import.meta.dirname, `dist`)));
 
-const upload = multer({dest: path.resolve(import.meta.dirname, `./images`)});
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.resolve(import.meta.dirname, `images`));
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage : storage });
 
 /*
 app.get(`/` ,(req, res, ) => {
   return res.send(`index.html`);
 });*/
 
-app.post(`/api/post/image`, upload.single(`image`), (req, res) => {
-  res.send(`file received`);
+function removeFileSync(directory) {
+  const files = fs.readdirSync(directory);
+  for (const file of files) {
+    const filePath = path.join(directory, file);
+    fs.unlinkSync(filePath);
+  }
+}
+removeFileSync(path.resolve(import.meta.dirname, `images`));
+app.post(`/api/post/image`, upload.single(`image`), (req, res, next) => {
+  console.log(`\nfile received`);
   console.log(req.file, req.body);
 });
 
